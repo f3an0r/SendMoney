c4f262da544ddb6403be0187d2a0c492372af85d
Alexander Shostak <f3anoralexander@gmail.com>
2020-10-30 20:06:39 +0300

    change type of fields

--- distribution_money/serializers.py
+++ distribution_money/serializers.py
@@ -3,15 +3,28 @@ from .models import User
 
 
 class DistributionMoneyFormSerializer(serializers.Serializer):
-    inn = serializers.CharField(label='ИНН', max_length=100, style={'placeholder': 'ИНН', 'autofocus': True})
-    summ = serializers.IntegerField(label='Сумма', style={'placeholder': 'Сумма'})
+    inn = serializers.CharField(label='ИНН', max_length=400, style={'placeholder': 'ИНН', 'autofocus': True},
+                                help_text="Введите ИНН через запятую ','")
+    summa = serializers.DecimalField(max_digits=50, decimal_places=2, label='Сумма', style={'placeholder': 'Сумма'})
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['users'] = serializers.ChoiceField(
+            label='Пользователь',
+            help_text='Выберите пользователя со счета которого необходимо списать средства',
             choices=[(choice.pk, choice.name) for choice in User.objects.all()]
         )
 
     def validate(self, data):
-        # raise serializers.ValidationError("finish must occur after start")
+        inns = data.get('inn', None)
+        data['inn'] = list()
+        try:
+            for inn in inns.split(','):
+                if inn.strip():
+                    data['inn'].append(int(inn))
+                else:
+                    continue
+        except:
+            raise serializers.ValidationError("inn is not valid")
         return data
+
