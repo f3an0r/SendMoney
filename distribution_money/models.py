381752e1642b3f6315601e823c181100c57352ad
Alexander Shostak <f3anoralexander@gmail.com>
2020-10-30 20:05:17 +0300

    add method for class User for add money to account and get money from account

--- distribution_money/models.py
+++ distribution_money/models.py
@@ -1,9 +1,33 @@
 from django.db import models
 
 class User(models.Model):
-    money = models.IntegerField()
+    money = models.DecimalField(max_digits=50, decimal_places=2)
     name = models.CharField(max_length=100)
     email = models.EmailField()
     inn = models.IntegerField(null=True)
 
-# Create your models here.
+    def add_money(self, summa):
+        """
+        Add money for user's account
+        :param summa: value for add
+        :return:
+        """
+        self.money += summa
+        self.save()
+
+    def get_money(self, summa):
+        """
+        Get money from the user's account
+        :param summa: the amount to be withdrawn from the account
+        :return: True - if summa is get
+        :return: False - if summa is not get
+        """
+        if not summa:
+            return False
+        if self.money >= summa:
+            self.money -= summa
+            self.save()
+            return True
+        else:
+            return False
+
